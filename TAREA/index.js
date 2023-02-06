1884fcd97eae5293484ecbabc790121de628cf18
Aguas7x <111818969+Aguas7x@users.noreply.github.com>
2023-02-05 20:09:28 -0800

    se agrega tarea

---
+++ TAREA/index.js
@@ -0,0 +1,77 @@
+const input_nombre = document.querySelector('#nombre');
+const button_submit = document.querySelector('#add');
+const content = document.querySelector('.content');
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const nombres = JSON.parse(localStorage.getItem('nombres'));
+    // ! si es falso me lo trae a true null = false 
+    if (!nombres) {
+        const p = document.createElement('p');
+        const text = document.createTextNode('No hay elementos');
+        p.appendChild(text)
+        content.appendChild(p)
+    } else {
+        if (nombres.length > 0) {
+            for (let i = 0; i < nombres.length; i++){
+              showNames(nombres, i);
+            }
+        } else {
+            const p = document.createElement('p');
+            const text = document.createTextNode('No hay elementos');
+    
+            p.appendChild(text);
+            content.appendChild(p);
+        }
+    }
+
+    button_submit.addEventListener('click', function(){
+        const nombres = JSON.parse(localStorage.getItem('nombres')) || [];
+        const nuevoNombre = input_nombre.value;
+        nombres.push(nuevoNombre);
+        localStorage.setItem('nombres', JSON.stringify(nombres));
+
+        content.innerHTML = '';
+
+        for (let i = 0; i < nombres.length; i++){
+            showNames(nombres, i);
+            
+        }
+    })
+});
+
+
+function showNames(nombres, i){
+    const div = document.createElement('div')
+    const p = document.createElement('p');
+    const text = document.createTextNode(nombres[i])
+    p.appendChild(text)
+
+    const button = document.createElement('button');
+    const textButton = document.createTextNode('Eliminar');
+    button.appendChild(textButton);
+
+    button.onclick = ( ) => {
+        eliminar(i)
+        
+    }
+
+    div.appendChild(p);
+    div.appendChild(button);
+
+    content.appendChild(div);
+}
+
+function eliminar (i) {
+
+    var nombres =JSON.parse(localStorage.getItem('nombres')) || [];
+    nombres = nombres.filter ( (value, index) => index !== i);
+
+    localStorage.setItem('nombres', JSON.stringify(nombres));
+    content.innerHTML = "";
+
+    for (let i = 0; i < nombres.length; i++) {
+        showNames(nombres, i);
+    }
+
+}
